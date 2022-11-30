ac4241384e4ec6f4b7e856b3d2912e40fe68ab99
GooX <gurgen.adiyan@sceon.am>
2022-11-30 19:49:24 +0000

    changes

--- network.js
+++ network.js
@@ -1,25 +1,13 @@
 const DsNetwork = require('ds2482-temperature');
 
-module.exports = (config, onData, onError) => {
+module.exports = config => {
   let device = '/dev/i2c-' + parseInt(config.i2cBus).toString();
   let address = parseInt(config.i2cAddress);
 
   const network = new DsNetwork({
     address,
-    device,
-    pollRate: parseInt(config.pollInterval)
+    device
   });
-
-  network
-    .init()
-    .then(() => {
-      network.on('data', onData);
-      network.on('error', onError);
-    })
-    .catch(err => {
-      onError(err);
-      network.init().then();
-    });
 
   return network;
 };
--- nodes.js
+++ nodes.js
@@ -40,9 +40,6 @@ module.exports = RED => {
     const network = instances[n.network];
 
     node.on('close', () => {
-      network.removeListener('data', onData);
-      network.removeListener('error', onError);
-
       listenerCount[n.network]--;
 
       if (listenerCount[n.network] === 0) {
@@ -55,57 +52,4 @@ module.exports = RED => {
   }
 
   RED.nodes.registerType('ds2482-network', NetworkSensors);
-
-  function Sensor(n) {
-    RED.nodes.createNode(this, n);
-    let node = this;
-
-    let networkConfig = RED.nodes.getNode(n.network);
-
-    if (!networkConfig) {
-      return;
-    }
-
-    function onData(data) {
-      let _msg = {
-        payload: data
-      };
-
-      node.send(_msg);
-    }
-
-    function onError(error) {
-      node.error(error.toString(), error);
-    }
-
-    const sensor = sensorFactory(
-      { ...networkConfig, rom: n.rom },
-      onData,
-      onError
-    );
-
-    function onInput(msg) {
-      sensor.readTemperature().then(data => {
-        node.send({
-          topic: n.rom,
-          payload: data
-        });
-      });
-    }
-
-    node.on('input', onInput);
-
-    node.on('close', () => {
-      // network.removeListener('data', onData);
-      // network.removeListener('error', onError);
-      // listenerCount[n.network]--;
-      // if (listenerCount[n.network] === 0) {
-      //   network.destroy();
-      //   delete instances[n.network];
-      //   delete instances[n.network];
-      // }
-    });
-  }
-
-  RED.nodes.registerType('ds2482-sensor', Sensor);
 };
